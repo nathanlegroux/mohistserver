f28f2888fa7d0a7953343584b982e651b640bc5c
⭐Nathan ⭐ <59396862+nathanlegroux@users.noreply.github.com>
2024-10-30 20:47:06 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,16 @@
-sed -i -e "/^rcon.port=/s/^rcon.port=.*/rcon.port=99$(echo \"{{SERVER_PORT}}\" | sed 's/^..//')/" \
--e "/^rcon.password=/s/^rcon.password=.*/rcon.password={{P_SERVER_UUID}}/" "server.properties" && \
-nohup java -Xms128M -Xmx{{SERVER_MEMORY}}M -Dterminal.jline=false -Dterminal.ansi=true $( [[ ! -f unix_args.txt ]] && printf %s \"-jar {{SERVER_JARFILE}}\" || printf %s \"@unix_args.txt\" ) > java.log 2>&1 & \
+# Extract SERVER_PORT and ensure it's properly handled
+SERVER_PORT_CLEANED=$(echo "{{SERVER_PORT}}" | sed 's/^..//')
+
+# Update server.properties using sed
+sed -i -e "/^rcon.port=/s/^rcon.port=.*/rcon.port=99${SERVER_PORT_CLEANED}/" \
+       -e "/^rcon.password=/s/^rcon.password=.*/rcon.password={{P_SERVER_UUID}}/" "server.properties"
+
+# Start the Java process with proper handling of SERVER_MEMORY and SERVER_JARFILE
+if [[ ! -f unix_args.txt ]]; then
+    nohup java -Xms128M -Xmx{{SERVER_MEMORY}}M -Dterminal.jline=false -Dterminal.ansi=true -jar "{{SERVER_JARFILE}}" > java.log 2>&1 &
+else
+    nohup java -Xms128M -Xmx{{SERVER_MEMORY}}M -Dterminal.jline=false -Dterminal.ansi=true @unix_args.txt > java.log 2>&1 &
+fi
+
+# Start the bore command
 nohup ./bore local {{SERVER_PORT}} --port {{SERVER_PORT}} --to play.timeslotter.fr > bore.log 2>&1 &
